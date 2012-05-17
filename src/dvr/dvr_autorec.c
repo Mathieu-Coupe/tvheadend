8bdf981fcc0e59fc575d21b874a91637bafb3c60
Mathieu Coupe <eagle.pounains@gmail.com>
2012-05-17 13:25:24 +0200

    Correct file header (with Öman)

--- src/dvr/dvr_autorec.c
+++ src/dvr/dvr_autorec.c
@@ -1,6 +1,6 @@
 /*
  *  tvheadend, Automatic recordings
- *  Copyright (C) 2010 Andreas Öman
+ *  Copyright (C) 2010 Andreas Ã–man
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
